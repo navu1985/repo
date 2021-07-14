1381ade6c0818a99858079959e4cb31fff11da30
nathakur <nathakur@NATHAKUR-IN.in.oracle.com>
2021-07-15 01:44:53 +0530

    hi

---
+++ spring-boot/product-catalog-microservice-1/src/main/java/com/example/services/ProductCatalogMicroservice1Application.java
@@ -0,0 +1,13 @@
+package com.example.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogMicroservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogMicroservice1Application.class, args);
+	}
+
+}
---
+++ spring-boot/product-catalog-microservice/src/main/java/com/example/services/ProductCatalogMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogMicroserviceApplication.class, args);
+	}
+
+}
